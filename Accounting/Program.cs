3c087c41a205f3dae91aa88ff84a6615951165ce
Glen Buck <glenbuck503@gmail.com>
2018-11-29 16:32:11 -0800

    dsfdf

---
+++ Accounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("We are counting on you!");
+            Console.Read();
+        }
+    }
+}
